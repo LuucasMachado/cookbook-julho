d05fe09f53cd019d7509221ba21764aedfaaf8ac
Vini <viniksantana5@gmail.com>
2019-07-16 22:11:37 -0300

    Inicio

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,25 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def create
+    @recipe = Recipe.create(params.require(:recipe).permit([:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method]))
+    redirect_to @recipe
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params.require(:recipe).permit([:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method]))
+    redirect_to @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
