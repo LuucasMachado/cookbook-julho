d5ffeba843b8e296ec2005deb4c25b368c4e2fb9
Vini <viniksantana5@gmail.com>
2019-07-17 22:08:52 -0300

    Finalizando validacao e relacionamento entre classes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,18 @@
 class RecipesController < ApplicationController
+  before_action :find_params, only: [:update, :edit, :show]
+  before_action :recipe_type_all, only: [:edit, :new]
   def index
     @recipes = Recipe.all
   end
 
   def create
-    @recipe = Recipe.create(params.require(:recipe).permit([:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method]))
-    redirect_to @recipe
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else 
+      flash[:alert] = "Você deve informar todos os dados da receita"
+      render :new
+    end
   end
 
   def new
@@ -13,16 +20,31 @@ class RecipesController < ApplicationController
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
-    @recipe.update(params.require(:recipe).permit([:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method]))
-    redirect_to @recipe
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      flash[:alert] = "Você deve informar todos os dados da receita"
+      render :edit
+    end
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def show
+  end
+
+  private
+
+  def recipe_type_all
+    @recipe_type = RecipeType.all
+  end
+
+  def find_params
     @recipe = Recipe.find(params[:id])
+  end
+    
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type_id, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
   end
 end
